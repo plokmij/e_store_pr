0ad3ea439ff15b95724658e10530ec2c0c465558
Samfan <samfan@live.com>
2024-07-29 17:58:02 +0530

    feat: add firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:e_store_pr/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -6,7 +8,13 @@ import 'features/authentication/data/repositories/auth_repository.dart';
 import 'features/authentication/data/repositories/user_repository.dart';
 import 'features/authentication/provider/auth_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => AuthProvider(
