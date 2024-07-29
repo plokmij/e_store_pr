d56637032a2e5d072f848a87a465e62267cf98e8
Samfan <samfan@live.com>
2024-07-29 13:02:59 +0530

    feat: add Product model for home feature

---
+++ lib/features/home/data/models/product.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Product {
+  int id;
+  String title;
+  String description;
+  double price;
+  double discountPercentage;
+  String thumbnail;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.discountPercentage,
+    required this.thumbnail,
+  });
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'] as int,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      price: map['price'] as double,
+      discountPercentage: map['discountPercentage'] as double,
+      thumbnail: map['thumbnail'] as String,
+    );
+  }
+
+  factory Product.fromJson(String source) =>
+      Product.fromMap(json.decode(source) as Map<String, dynamic>);
+}
